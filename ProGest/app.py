9c0b59eb6a7fd56b3be6cdcbae6ddae8c42c931c
Mori-jane <rianarabelahatra@gmail.com>
2024-07-04 11:13:08 +0300

    for

--- ProGest/app.py
+++ ProGest/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index(): #afficher formulaire de connexion
-    return render_template("index.html")
+    return render_template("chatbox.html")
 
 
 @app.route('/traitement', methods=['POST'])
