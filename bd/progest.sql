3d606927c7c042e1cddfbb0f788019038ae03855
Johanna452 <joandriam16@gmail.com>
2024-07-08 13:30:15 +0300

    nouvelle base de données

--- bd/progest.sql
+++ bd/progest.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : jeu. 04 juil. 2024 à 04:59
+-- Généré le : lun. 08 juil. 2024 à 08:40
 -- Version du serveur : 8.0.31
 -- Version de PHP : 8.0.26
 
@@ -32,6 +32,8 @@ CREATE TABLE IF NOT EXISTS `projet` (
   `nomProjet` varchar(50) NOT NULL,
   `Description` text NOT NULL,
   `Objectifs` text NOT NULL,
+  `DateDeb` date NOT NULL,
+  `DateFin` date NOT NULL,
   PRIMARY KEY (`nomProjet`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
@@ -39,8 +41,53 @@ CREATE TABLE IF NOT EXISTS `projet` (
 -- Déchargement des données de la table `projet`
 --
 
-INSERT INTO `projet` (`nomProjet`, `Description`, `Objectifs`) VALUES
-('ProGest', 'Application web de gestion de projet', 'Aider les équipes à s\'organiser');
+INSERT INTO `projet` (`nomProjet`, `Description`, `Objectifs`, `DateDeb`, `DateFin`) VALUES
+('ProGest', 'Application web de gestion de projet', 'Aider les équipes à s\'organiser', '0000-00-00', '0000-00-00');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `taches`
+--
+
+DROP TABLE IF EXISTS `taches`;
+CREATE TABLE IF NOT EXISTS `taches` (
+  `nomTache` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `DateDeb` date NOT NULL,
+  `DateFin` date NOT NULL,
+  `statut` text NOT NULL,
+  `priorite` text NOT NULL,
+  `nomProjet` varchar(50) NOT NULL,
+  PRIMARY KEY (`nomTache`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `usertask`
+--
+
+DROP TABLE IF EXISTS `usertask`;
+CREATE TABLE IF NOT EXISTS `usertask` (
+  `id_usertask` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(30) NOT NULL,
+  `nomTache` varchar(100) NOT NULL,
+  PRIMARY KEY (`id_usertask`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user_project`
+--
+
+DROP TABLE IF EXISTS `user_project`;
+CREATE TABLE IF NOT EXISTS `user_project` (
+  `id_user_project` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(30) NOT NULL,
+  `nomProjet` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_user_project`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
